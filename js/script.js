b5835445274626d5c97cf03bd4ef786492b50e25
Alexandr <52231545+chalma42@users.noreply.github.com>
2021-12-27 12:29:48 +0400

    удалил лишнее

--- js/script.js
+++ js/script.js
@@ -1,8 +1,37 @@
-$(document).ready(function(){
-    $(".main-slider").slick({
-        // autoplay: true,
-        autoplaySpeed: 7000,
-        infinite: true
-      });
+const swiper = new Swiper('.swiper', {
+  // Default parameters
+  slidesPerView: 1,
+  autoplay: {
+    delay: 5000,
+  },
+  // Disable preloading of all images
+  preloadImages: false,
+  // Enable lazy loading
+  lazy: true,
 });
+
+//Открываем вкладку новости по умолчанию
+document.querySelector('#news').style.display = "block";
+//Открываем табы
+function openTab(evt, cityName) {
+  let i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("content-list__content");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("content-list__link");
+  for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
 
