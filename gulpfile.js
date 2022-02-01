1aedf537428e28059ba3860e0c2a1b2c4b824283
Alexandr <52231545+chalma42@users.noreply.github.com>
2022-02-01 17:49:04 +0400

    добавилгитигнор

--- gulpfile.js
+++ gulpfile.js
@@ -15,8 +15,8 @@ function browsersync() {
 
 function styles() {
     return src('app/scss/style.scss')
-        .pipe(sass({outputStyle: 'compressed' }))
-        .pipe(concat('style.min.css')) //переименовывает конечный файл .css
+        .pipe(sass())
+        //.pipe(concat('style.min.css')) //переименовывает конечный файл .css
         .pipe(dest('app/css'))
         .pipe(browserSync.stream());
 }
@@ -30,4 +30,4 @@ exports.styles = styles;
 exports.watching = watching;
 exports.browserSync = browsersync;
 
-exports.default = parallel(browsersync, watching)
+exports.default = parallel(browsersync, watching);
