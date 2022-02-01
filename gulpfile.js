cb57dd8d4f4806650f10dee7f5d3c3be80cb0024
Alexandr <52231545+chalma42@users.noreply.github.com>
2022-02-01 17:17:49 +0400

    настрйока галп 2

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const { src, dest, watch, parallel } = require('gulp');
+
+const sass = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+
+function browsersync() {
+    browserSync.init({
+        server: {
+            baseDir: "app/"
+        }
+    });
+}
+
+
+function styles() {
+    return src('app/scss/style.scss')
+        .pipe(sass({outputStyle: 'compressed' }))
+        .pipe(concat('style.min.css')) //переименовывает конечный файл .css
+        .pipe(dest('app/css'))
+        .pipe(browserSync.stream());
+}
+
+function watching() {
+    watch(['app/scss/**/*.scss'], styles)
+    watch(['app/*.html']).on('change', browserSync.reload);
+}
+
+exports.styles = styles;
+exports.watching = watching;
+exports.browserSync = browsersync;
+
+exports.default = parallel(browsersync, watching)
