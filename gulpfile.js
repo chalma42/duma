05029ed953b53902e47dee692307e42f2560d05a
Alexandr <52231545+chalma42@users.noreply.github.com>
2022-02-02 15:20:02 +0400

    ctujlyz gfhdrf ufkg

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 const { src, dest, watch, parallel } = require('gulp');
 
-const sass = require('gulp-sass')(require('sass'));
-const concat = require('gulp-concat');
-const browserSync = require('browser-sync').create();
-const uglify = require('gulp-uglify-es').default;
+const sass         = require('gulp-sass')(require('sass'));
+const concat       = require('gulp-concat');
+const browserSync  = require('browser-sync').create();
+const uglify       = require('gulp-uglify-es').default;
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin     = require('gulp-imagemin');
 
 function browsersync() {
     browserSync.init({
@@ -12,12 +14,31 @@ function browsersync() {
         }
     });
 }
+function images() {
+    return src('app/img/**/*')
+        .pipe(imagemin([
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.mozjpeg({quality: 75, progressive: true}),
+            imagemin.optipng({optimizationLevel: 5}),
+            imagemin.svgo({
+                plugins: [
+                    {removeViewBox: true},
+                    {cleanupIDs: false}
+                ]
+            })
+        ]))
+        .pipe(dest('dist/img'))
+}
 
 function scripts() {
     return src([
-        '',
-        'main.js'
+        //'app/js/swiper-bundle.min.js',
+        'app/js/main.js'
     ])
+    //.pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest('app/js'))
+    .pipe(browserSync.stream())
 }
 
 
@@ -25,17 +46,35 @@ function styles() {
     return src('app/scss/style.scss')
         .pipe(sass())
         //.pipe(concat('style.min.css')) //переименовывает конечный файл .css
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 10 versions'],
+            grid: true
+        }))
         .pipe(dest('app/css'))
         .pipe(browserSync.stream());
 }
 
+function build() {
+    return src([
+        'app/css/style.css',
+        'app/fonts/**/*',
+        'app/js/**/*.js',
+        'app/*.html'
+    ], {base: 'app'})
+    .pipe(dest('dist'))
+}
+
 function watching() {
     watch(['app/scss/**/*.scss'], styles)
+    watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts)
     watch(['app/*.html']).on('change', browserSync.reload);
 }
 
 exports.styles = styles;
 exports.watching = watching;
 exports.browserSync = browsersync;
+exports.scripts = scripts;
+exports.build = build;
+exports.images = images;
 
 exports.default = parallel(browsersync, watching);
