5c10dfcdd087f4ff44e49486462fcd2b2c90eecc
Alexandr <52231545+chalma42@users.noreply.github.com>
2022-02-21 00:10:04 +0400

    домашняя верстка

--- app/js/script.js
+++ app/js/script.js
@@ -12,6 +12,8 @@ const swiper = new Swiper('.swiper', {
   
 //Открываем вкладку новости по умолчанию
 document.querySelector('#news').style.display = "block";
+//
+document.querySelector('#deputats').style.display = "block";
 //Открываем табы
 function openTab(evt, value) {
   let i;
@@ -20,6 +22,21 @@ function openTab(evt, value) {
       tabcontent[i].style.display = "none";
   }
   let tablinks = document.getElementsByClassName("content-list__link");
+  for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(value).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+//Открываем табы2
+function openTab2(evt, value) {
+  let i;
+  let tabcontent = document.getElementsByClassName("my-deputat__content");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+  }
+  let tablinks = document.getElementsByClassName("my-deputat__link");
   for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
